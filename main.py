b88764bdf3e41c698968ff53c38db78c731ff09f
Elisabeth Fauquet <e.fauquet3@gmail.com>
2022-02-27 21:59:26 +0100

    added FileNotFoundException

--- main.py
+++ main.py
@@ -22,23 +22,27 @@ class Player:
         self.charisma = 10
 
     def load_player(self):
-        f = open(PLAYER_DIR + self.name + ".json", "r")
-        dic_player = json.load(f)
-        f.close()
-        print("loading player %s " % (dic_player["name"]))
-        self.pc_class = dic_player["pc_class"]
-        self.level = dic_player["level"]
-        self.race = dic_player["race"]
-        self.speed = dic_player["speed"]
-        self.hit_points = dic_player["hit_points"]
-        self.hit_points_max = dic_player["hit_points_max"]
-        self.armor_class = dic_player["armor_class"]
-        self.strength = dic_player["strength"]
-        self.dexterity = dic_player["dexterity"]
-        self.constitution = dic_player["constitution"]
-        self.intelligence = dic_player["intelligence"]
-        self.wisdom = dic_player["wisdom"]
-        self.charisma = dic_player["charisma"]
+        file_path = PLAYER_DIR + self.name + ".json"
+        try:
+            f = open(file_path, "r")
+            dic_player = json.load(f)
+            f.close()
+            print("loading player %s " % (dic_player["name"]))
+            self.pc_class = dic_player["pc_class"]
+            self.level = dic_player["level"]
+            self.race = dic_player["race"]
+            self.speed = dic_player["speed"]
+            self.hit_points = dic_player["hit_points"]
+            self.hit_points_max = dic_player["hit_points_max"]
+            self.armor_class = dic_player["armor_class"]
+            self.strength = dic_player["strength"]
+            self.dexterity = dic_player["dexterity"]
+            self.constitution = dic_player["constitution"]
+            self.intelligence = dic_player["intelligence"]
+            self.wisdom = dic_player["wisdom"]
+            self.charisma = dic_player["charisma"]
+        except FileNotFoundException:
+            print(file_path + " not found")
 
     def __str__(self):
         return "%s - level %d %s\n\t%s\n\tSpeed %dft\n\tHP %d / %d\n\tAC %d \n\tSTR %d | DEX %d | CON %d | INT %d | WIS %d | CHA %d\n" % (
@@ -56,14 +60,18 @@ class Campaign:
         self.players[player.name] = player
 
     def load_campaign(self):
-        f = open(CAMPAIGN_DIR + campaign.campaign_name + ".json", "r")
-        dic_campaign = json.load(f)
-        f.close()
-        for key in dic_campaign:
-            new_player = Player(key)
-            new_player.load_player()
-            new_player.hit_points = dic_campaign[key]
-            self.add_player(new_player)
+        file_path = CAMPAIGN_DIR + campaign.campaign_name + ".json"
+        try:
+            f = open(file_path, "r")
+            dic_campaign = json.load(f)
+            f.close()
+            for key in dic_campaign:
+                new_player = Player(key)
+                new_player.load_player()
+                new_player.hit_points = dic_campaign[key]
+                self.add_player(new_player)
+        except FileNotFoundError:
+            print(file_path + " not found")
 
     def __str__(self):
         res = "Campaign %s\n" % self.campaign_name
